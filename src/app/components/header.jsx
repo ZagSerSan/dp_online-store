11622a79a218f897ad1b1c5753638182cf892ffe
ZagSerSan <yan.steys1997@gmail.com>
2023-06-10 13:59:48 +0200

    dropMenu

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 // css
 import './header.css'
@@ -7,17 +7,39 @@ import LOGO from '../assets/img/logo/logo.png'
 import Icon from './common/icon'
 
 const Header = () => {
+  const [dropMenu, setDropMenu] = useState(false)
+
   return (
     <header className='container header'>
       <div className='header-logo'>
         <img src={LOGO} alt="logo" />
       </div>
-      <nav className='header-nav'>
-        <Link to="/" className='header-nav__link'>Home</Link>
-        <Link to="/category" className='header-nav__link'>Category</Link>
-        <Link to="/delivery" className='header-nav__link'>Delivery</Link>
-        <Link to="/about" className='header-nav__link'>About us</Link>
-        <Link to="/contacts" className='header-nav__link'>Contacts</Link>
+      <nav>
+        <ul className='header-nav'>
+          <li className='header-nav__link'><Link to="/">Home</Link></li>
+          <li className='header-nav__link'>
+            <Link
+              to="/category"
+              onMouseEnter={() => setDropMenu(true)}
+              onMouseLeave={() => setDropMenu(false)}
+            >
+              Category
+            </Link>
+            {dropMenu && <div
+              onMouseEnter={() => setDropMenu(true)}
+              onMouseLeave={() => setDropMenu(false)}
+              className='drop-menu'
+              // className={'drop-menu active' + (dropMenu ? ' active' : '')}
+            >
+              <Link className='drop-menu__link' to='/category/forher'>For her</Link>
+              <Link className='drop-menu__link' to='/category/forhim'>For him</Link>
+              <Link className='drop-menu__link' to='/category/forauto'>For auto</Link>
+            </div>}
+          </li>
+          <li className='header-nav__link'><Link to="/delivery">Delivery</Link></li>
+          <li className='header-nav__link'><Link to="/about">About us</Link></li>
+          <li className='header-nav__link'><Link to="/contacts">Contacts</Link></li>
+        </ul>
       </nav>
       <div className='header-cart'>
         <Icon className='header-cart__icon' id='search'/>
