4ed3b6d1cc5ffef3daaf1c92bf92311ce8f264f8
ZagSerSan <yan.steys1997@gmail.com>
2023-06-11 16:21:48 +0200

    subcategory logic

--- src/app/components/categoryNav.jsx
+++ src/app/components/categoryNav.jsx
@@ -6,21 +6,25 @@ import IMG_DOG from '../assets/img/category-nav/dog.jpg'
 import IMG_CAT from '../assets/img/category-nav/cat.jpg' 
 import IMG_FISH from '../assets/img/category-nav/fish.jpg' 
 
-const CategoryNav = () => {
+const CategoryNav = ({ type }) => {
+
   return (
-    <div className='category-nav container'>
-      <Link to='/category/fordog' className="category-nav-item">
-        <img src={IMG_DOG} alt="for dog" />
-        <h3>Dogs Food</h3>
-      </Link>
-      <Link to='/category/forcat' className="category-nav-item">
-        <img src={IMG_CAT} alt="for cat" />
-        <h3>Cats Food</h3>
-      </Link>
-      <Link to='/category/forfish' className="category-nav-item">
-        <img src={IMG_FISH} alt="for fish" />
-        <h3>Fishs Food</h3>
-      </Link>
+    <div className="container">
+      <div className={'category-nav' + (type ? ' small' : '')}>
+        <Link to='/category/fordog' className={'category-nav-item' + (type === 'fordog' ? ' active' : '')}>
+          <img src={IMG_DOG} alt="for dog" />
+          <h3>Dogs Food</h3>
+        </Link>
+        <Link to='/category/forcat' className={'category-nav-item' + (type === 'forcat' ? ' active' : '')}>
+          <img src={IMG_CAT} alt="for cat" />
+          <h3>Cats Food</h3>
+        </Link>
+        <Link to='/category/forfish' className={'category-nav-item' + (type === 'forfish' ? ' active' : '')}>
+          <img src={IMG_FISH} alt="for fish" />
+          <h3>Fishs Food</h3>
+        </Link>
+      </div>
+      {type && <h1>Category: {type}</h1>}
     </div>
   )
 }
--- src/app/layouts/category.jsx
+++ src/app/layouts/category.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 import CategoryNav from '../components/categoryNav'
 
 const Category = () => {
+  const { type } = useParams()
+
   return (
     <div className="container">
-      <CategoryNav />
+      <CategoryNav type={type}/>
+      {!type && <h1>товары из разных категорий</h1>}
     </div>
   )
 }
