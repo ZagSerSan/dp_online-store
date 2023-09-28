73d2472e57169d9b69c6107f7c79241bd9505b83
ZagSerSan <yan.steys1997@gmail.com>
2023-09-28 19:20:34 +0200

    upd mockData, bookmark func

--- client/src/components/common/product/productItem.jsx
+++ client/src/components/common/product/productItem.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Icon from '../icon'
+import useStore from '../../../store/createStore'
+import productService from '../../../service/product.service'
 
 const ProductItem = ({ item, onClick }) => {
   const { _id, name, preview, title, price, type } = item
   const navigate = useNavigate()
+  const { setBookmarkForProduct } = useStore()
 
   const openItemPage = (e) => {
     e.stopPropagation()
     navigate(`/category/${type}/${_id}`)
+  }
+
+  const toggleBookmark = async (e, item) => {
+    // console.log('e.target :>> ', e.target)
+    // console.log('item :>> ', item)
+    e.stopPropagation()
+    const newItemData = {
+      ...item,
+      bookmark: item.bookmark === true ? false : true
+    }
+    try {
+      const { content } = await productService.toggleBookmark(newItemData)
+      setBookmarkForProduct(content)
+    } catch (e) {
+      console.log('e :>> ', e)
+    }
   }
 
   return (
@@ -26,9 +45,12 @@ const ProductItem = ({ item, onClick }) => {
       </div>
 
       <div className="popular-item__content">
-        <h4 className="popular-item__title">
-        <Link to={`/category/${item.type}/_id`}>{name}</Link>
-        </h4>
+        <div className="popular-item__title">
+          <Link to={`/category/${item.type}/${item._id}`}>{name}</Link>
+          <button onClick={(e) => toggleBookmark(e, item)} className='w-[24px] h-[24px] z-10'>
+            <Icon id='heart' fill={item.bookmark && '#000'}/>
+          </button>
+        </div>
         <p className="popular-item__price">${price}.00</p>
       </div>
     </div>
