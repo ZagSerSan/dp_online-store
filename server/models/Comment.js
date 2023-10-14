951f6f1328d4069e147a988416139cf539589289
ZagSerSan <yan.steys1997@gmail.com>
2023-10-14 11:54:03 +0200

    rate for comments

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,11 +1,16 @@
 const { Schema, model } = require('mongoose')
 
 const schema = new Schema({
+	// productId: {type: Schema.Types.ObjectId, ref: 'Product', required: true},
+	// userId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+	productId: {type: String, required: true},
+	userId: {type: String, required: true},
   content: {type: String, required: true},
+  name: {type: String},
+  email: {type: String},
+  rate: {type: String},
   // pageId - на чьей странице находится комментарий
-	pageId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   // userId - id того, кто оставил комментарий
-	userId: {type: Schema.Types.ObjectId, ref: 'User', required: true}
 }, {
   timestamps: {createdAt: 'created_at'}
 })
