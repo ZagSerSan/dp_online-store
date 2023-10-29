eae7cb54029b6d680d8fb0467f703d4f768fec30
ZagSerSan <yan.steys1997@gmail.com>
2023-10-29 13:17:49 +0100

    editProduct -> infoDanta is done

--- client/src/components/pages/admin/createAndEditPages/editProduct.jsx
+++ client/src/components/pages/admin/createAndEditPages/editProduct.jsx
@@ -1,59 +1,142 @@
-import React, { useState, useEffect } from 'react'
-import Icon from '../../../common/icon'
-import './css/editProduct.css'
-import { Navigate, useParams } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import './css/createProduct.css'
+import { filesValidator } from '../../../../utils/filesValidator'
+import ProductService from '../../../../service/product.service'
 import useStore from '../../../../store/createStore'
-import SettingProductContent from './settingProductContent'
+import EditProductConfig from './editProductConfig'
 
 const EditProduct = () => {
+  const { updateProduct, productsEntity } = useStore()
+  const { productId } = useParams()
   const [settingItemState, setSettingItemState] = useState(1)
   const settingItems = [
-    {number: 1, contentType: 'info', title: 'Edit your account information'},
-    {number: 2, contentType: 'images', title: 'Change your password'},
-    {number: 3, contentType: 'comments', title: 'Delete users comments'}
+    {number: 1, contentType: 'info', title: 'Add product information'},
+    {number: 2, contentType: 'options', title: 'Add product options'},
+    {number: 3, contentType: 'images', title: 'Add product images'}
   ]
-  const { productsEntity } = useStore()
-  const { productId } = useParams()
+  const [newProdData, setNewProdData] = useState({})
+  const [productType, setProductType] = useState()
 
-  const editedProduct = productsEntity
-    ? productsEntity.find(product => product._id === productId)
-    : null
-
-  const toggleSettingItem = (settingItemId) => {
+  const toggleSettingItem = (e, settingItemId) => {
+    e.stopPropagation()
     setSettingItemState(settingItemId)
   }
 
-  // useEffect(() => {
-  //   if (!usersLoaded) {
-  //     loadUsersList()
-  //   }
-  // }, [usersEntity, usersLoaded])
+  const handleSubmit = async (e, contentType, data) => {
+    e.preventDefault()
 
-  // if (!authorizated) {
-  //   return <Navigate to='/home'/>
-  // }
+    switch (contentType) {
+      case 'info':
+        //? setProductType(data.type)
+        //? setNewProdData(prev => ({ ...prev, ...data }))
+
+        const editedProduct = productsEntity.find(product => product._id === productId)
+        // проверять название для формирования папок на сервере
+        if (editedProduct.type !== data.type) {
+
+          const existedProduct = productsEntity.find(product => product.name.toLowerCase() === data.name.toLowerCase())
+          // если такое название уже есть в таком типе продуктов
+          if (existedProduct && existedProduct.type === data.type) {
+            console.log('Такой продукт уже есть, ничё не делать')
+
+          } else {
+            console.log('Такого продукта нету, можно менять папку, update')
+            updateProduct({_id: productId, ...data})
+          }
+        } else {
+          console.log('тип не менялся, просто update')
+          updateProduct({_id: productId, ...data})
+        }
+        
+        // отправка на сервер и обновление продукта
+        // updateProduct({_id: productId, ...data})
+        break;
+
+      case 'options':
+        for (let i = 0; i < Object.keys(data).length; i++) {
+          let index = i + 1
+          let optionTypeName = `option_${index}`
+          data[optionTypeName].options = Object.values(data[optionTypeName].options)
+        }
+        const modalOptionTypes = Object.values(data)
+        setNewProdData(prev => ({...prev, modalOptionTypes}))
+        break;
+
+      case 'images':
+        // images validate
+        const isNotValid = filesValidator(data)
+        if (isNotValid) return
+
+        // формируем data для отправки на сервер
+        const filesName = {
+          preview: [],
+          sliders: [],
+          dots: [],
+          intro: []
+        }
+        const files = {}
+        // индекс для перечня файлов
+        let customIndex = 0
+
+        // получения названий файлов
+        Object.keys(data.images).forEach((key, index) => {
+          for (let y = 0; y < data.images[key].length; y++) {
+            filesName[key] = [...filesName[key], data.images[key][y].name]
+          }
+        })
+        // получения файлов отдельно
+        Object.keys(data.images).forEach((key, index) => {
+          for (let y = 0; y < data.images[key].length; y++) {
+            files[customIndex] = data.images[key][y]
+            customIndex += 1
+          }
+        })
+
+        const newProdData_updated = {
+          ...newProdData,
+          introSlider: data.introSlider,
+          filesName
+        }
+
+        // console.log('newProdData_updated :>> ', newProdData_updated)
+        // console.log('files :>> ', files)
+        
+        ProductService.createProductImages(
+          files,
+          {productName: newProdData.name, type: newProdData.type}
+        )
+        createNewProduct(newProdData_updated)
+        navigate('/admin/products')
+        break;
+      default:
+        break;
+    }
+  }
 
   return (
     <div className="my-container">
-      {editedProduct
-        ? (<div className='user-page'>
-            {settingItems.map(settingItem => (
-              <div key={settingItem.number} className={"setting-item" + (settingItemState === settingItem.number ? ' active' : '')}>
-                <div className="setting-item-clicker" onClick={() => toggleSettingItem(settingItem.number)}>
-                  <div className='setting-item-clicker__number'>{settingItem.number}</div>
-                  <div className='setting-item-clicker__title'>{settingItem.title}</div>
-                </div>
-                <div className="setting-item-content">
-                  <SettingProductContent
-                    contentType={settingItem.contentType}
-                    editedProduct={editedProduct}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>)
-        : <Icon id='loader'/>
-      }
+      <div className='user-page'>
+        {settingItems.map(settingItem => (
+          <div key={settingItem.number} className={"setting-item" + (settingItemState === settingItem.number ? ' active' : '')}>
+            <div
+              className="setting-item-clicker"
+              onClick={(e) => toggleSettingItem(e, settingItem.number)}
+            >
+              <div className='setting-item-clicker__number'>{settingItem.number}</div>
+              <div className='setting-item-clicker__title'>{settingItem.title}</div>
+            </div>
+            <div className="setting-item-content">
+              <EditProductConfig
+                contentType={settingItem.contentType}
+                toggleSettingItem={toggleSettingItem}
+                handleSubmit={handleSubmit}
+                productType={productType}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ client/src/components/pages/admin/editProduct.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import Icon from '../../common/icon'
+import './css/editProduct.css'
+import { Navigate, useParams } from 'react-router-dom'
+import useStore from '../../../store/createStore'
+import SettingProductContent from './settingProductContent'
+
+const EditProduct = () => {
+  const [settingItemState, setSettingItemState] = useState(1)
+  const settingItems = [
+    {number: 1, contentType: 'info', title: 'Edit your account information'},
+    {number: 2, contentType: 'images', title: 'Change your password'},
+    {number: 3, contentType: 'comments', title: 'Delete users comments'}
+  ]
+  const { productsEntity } = useStore()
+  const { productId } = useParams()
+
+  const editedProduct = productsEntity
+    ? productsEntity.find(product => product._id === productId)
+    : null
+
+  const toggleSettingItem = (settingItemId) => {
+    setSettingItemState(settingItemId)
+  }
+
+  // useEffect(() => {
+  //   if (!usersLoaded) {
+  //     loadUsersList()
+  //   }
+  // }, [usersEntity, usersLoaded])
+
+  // if (!authorizated) {
+  //   return <Navigate to='/home'/>
+  // }
+
+  return (
+    <div className="my-container">
+      {editedProduct
+        ? (<div className='user-page'>
+            {settingItems.map(settingItem => (
+              <div key={settingItem.number} className={"setting-item" + (settingItemState === settingItem.number ? ' active' : '')}>
+                <div className="setting-item-clicker" onClick={() => toggleSettingItem(settingItem.number)}>
+                  <div className='setting-item-clicker__number'>{settingItem.number}</div>
+                  <div className='setting-item-clicker__title'>{settingItem.title}</div>
+                </div>
+                <div className="setting-item-content">
+                  <SettingProductContent
+                    contentType={settingItem.contentType}
+                    editedProduct={editedProduct}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>)
+        : <Icon id='loader'/>
+      }
+    </div>
+  )
+}
+
+export default EditProduct
