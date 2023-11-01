e017963ed170524bc6673c1b4e31d07ac2eae887
ZagSerSan <yan.steys1997@gmail.com>
2023-11-01 16:48:00 +0100

    todo: createUser func

--- client/src/components/pages/admin/adminPage.jsx
+++ client/src/components/pages/admin/adminPage.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import './css/adminPage.css'
 import UserTabPage from './userTabPage'
 import ProductsTabPage from './productsTabPage'
-import { useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from 'react-router-dom'
+import userStore from '../../../store/userStore'
 
 const AdminPage = ({ tabState }) => {
+  const { authedUser } = userStore()
   const navigate = useNavigate()
   // users, products
   const tabs = ['users', 'products']
@@ -15,20 +17,22 @@ const AdminPage = ({ tabState }) => {
 
   return (
     <div className='admin-page'>
-
-    <div className="my-container">
-      <div className="admin-page-tabs">
-        {tabs.map(tab => (
-          <button key={tab} className={tab === tabState ? ' active' : ''} onClick={() => toggleTabs(tab)}>{tab}</button>
-        ))}
-      </div>
-      <div className="admin-page-content">
-        {tabState === 'users'
-          ? <UserTabPage />
-          : <ProductsTabPage />
-        }
-      </div>
-    </div>
+      {authedUser && authedUser.admin
+        ? <div className="my-container">
+        <div className="admin-page-tabs">
+          {tabs.map(tab => (
+            <button key={tab} className={tab === tabState ? ' active' : ''} onClick={() => toggleTabs(tab)}>{tab}</button>
+          ))}
+        </div>
+        <div className="admin-page-content">
+          {tabState === 'users'
+            ? <UserTabPage />
+            : <ProductsTabPage />
+          }
+        </div>
+          </div>
+        : <Navigate to='/home'/>
+      }
     </div>
 
   )
--- client/src/service/user.service.js
+++ client/src/service/user.service.js
@@ -9,15 +9,60 @@ const userService = {
     return data
   },
   getCurrentUser: async () => {
-    const {data} = await httpService.get(userEndpoint + localStorageService.getUserId())
+    const url = userEndpoint + localStorageService.getUserId()
+    const accessToken = `Bearer ${localStorageService.getAccessToken()}`
+    const {data} = await httpService.get(url,
+      {
+        headers: {
+        "Authorization": accessToken
+      }
+    })
     return data
   },
+  createUser: async (payload, role = 'register') => {
+    try {
+      const url = 'signUp'
+      const { data } = await httpAuth.post(url, payload)
+      if (role === 'register') {
+        localStorageService.setTokens(data)
+      }
+      toast.success("User has been created!")
+      return data
+    } catch (error) {
+      const errorType = error.response.data.error.message
+      console.log('err', error)
+      if (errorType === "EMAIL_EXISTS") {
+        toast.error("Email exists!")
+      } else {
+        toast.error("User not created..")
+      }
+    }
+  },
+  // : async (newUserData) => {
+  //   const url = ''
+  //   const accessToken = `Bearer ${localStorageService.getAccessToken()}`
+  //   const { data } = await httpService.post(
+  //     productEndpoint + url,
+  //     newUserData,
+  //     {headers: {"Authorization": accessToken}}
+  //   )
+  //   return data
+  // },
   updateUser: async (updUserData) => {
-    const { data } = await httpService.put(userEndpoint + updUserData._id, updUserData)
+    const accessToken = `Bearer ${localStorageService.getAccessToken()}`
+    const { data } = await httpService.put(
+      userEndpoint + updUserData._id,
+      updUserData,
+      {headers: {"Authorization": accessToken}}
+    )
     return data
   },
   deleteUser: async (userId) => {
-    const {data} = await httpService.delete(userEndpoint + userId)
+    const accessToken = `Bearer ${localStorageService.getAccessToken()}`
+    const {data} = await httpService.delete(
+      userEndpoint + userId,
+      {headers: {"Authorization": accessToken}}
+    )
     return data
   }
 }
