28618541d49f36837cf4735c808a4c29b3285f69
Serhii Zahorskyi <yan.steys1997@gmail.com>
2024-01-31 20:18:53 +0100

    todo: timer

--- client/src/components/ui/bestProduct.jsx
+++ client/src/components/ui/bestProduct.jsx
@@ -1,13 +1,72 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import _ from 'lodash'
 import './css/bestProduct.css'
 import productStore from '../../store/productStore'
+// todo = timer
+import moment from 'moment'
 
 const BestProduct = () => {
   const { productsEntity } = productStore()
   // продукт, максимального рейтинга
   const maxRatingProduct = _.orderBy(productsEntity, ['rate'], ['desc'])[0]
+
+// todo = timer
+  const counterItems = [
+    {type: 'DD', name: 'Days'},
+    {type: 'HH', name: 'Hours'},
+    {type: 'mm', name: 'Min'},
+    {type: 'ss', name: 'Sec'},
+  ]
+
+  
+  const nowDate = moment(Date.now())
+  const futureDate = moment('2024-01-31T22:00:00')
+  const [timeDiff, setTimeDiff] = useState()
+
+  // let updateTimer = () => {
+  //   if (timeDiff > 0) {
+  //     setTimeDiff(moment.utc(futureDate.diff(Date.now())))
+  //   } else {
+  //     // clearInterval(timer);
+  //     console.log('else')
+  //   }
+  // }
+  // let timer = setInterval(updateTimer, 1000);
+  // console.log('timer :>> ', timer)
+
+  useEffect(() => {
+
+    setTimeDiff(moment.utc(futureDate.diff(nowDate)))
+
+    // let updateTimer = () => {
+    //   if (timeDiff > 0) {
+    //     setTimeDiff(prev => prev - 1000)
+    //   } else {
+    //     // clearInterval(timer);
+    //     console.log('else')
+    //   }
+    // }
+    // let timer = setInterval(updateTimer, 1000)
+    // updateTimer()
+
+  }, [])
+
+  // let updateTimer = () => {
+  //   if (timeDiff > 0) {
+  //     setTimeDiff(moment.utc(futureDate.diff(Date.now())))
+  //   } else {
+  //     // clearInterval(timer);
+  //     console.log('else')
+  //   }
+  // }
+  // let timer = setInterval(updateTimer, 1000);
+  // console.log('timer :>> ', timer)
+  // updateTimer()
+
+  const getTimeValue = (formatType) => {
+    return timeDiff.format(formatType)
+  }
 
   return (<>
     {maxRatingProduct && (
@@ -37,22 +96,14 @@ const BestProduct = () => {
                 Lorem ipsum dolor sit amet, co adipisicing elit, sed do eiusmod tempor incididunt labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercita ullamco laboris nisi ut aliquip ex ea commodo
               </p>
               <div className="best-product-content-counter">
-                <div className="best-product-content-counter-item">
-                  <p className="best-product-content-counter-item__number">0</p>
-                  <p className="best-product-content-counter-item__name">Days</p>
-                </div>
-                <div className="best-product-content-counter-item">
-                  <p className="best-product-content-counter-item__number">0</p>
-                  <p className="best-product-content-counter-item__name">Hour</p>
-                </div>
-                <div className="best-product-content-counter-item">
-                  <p className="best-product-content-counter-item__number">00</p>
-                  <p className="best-product-content-counter-item__name">Min</p>
-                </div>
-                <div className="best-product-content-counter-item">
-                  <p className="best-product-content-counter-item__number">00</p>
-                  <p className="best-product-content-counter-item__name">Sec</p>
-                </div>
+                {timeDiff && counterItems.map(formatItem => (
+                  <div key={formatItem.type} className="best-product-content-counter-item">
+                    <p className="best-product-content-counter-item__number">
+                      {getTimeValue(formatItem.type)}
+                    </p>
+                    <p className="best-product-content-counter-item__name">{formatItem.name}</p>
+                  </div>
+                ))}
               </div>
               <Link
                 className="best-product-content__shop-btn"
