6f8cde9bd00d19e4460f83ac527bed0ea81419c9
Serhii Zahorskyi <yan.steys1997@gmail.com>
2025-02-28 14:07:50 +0100

    fixed CategoryNav bugs, todo css animations

--- client/src/components/ui/categoryNav.jsx
+++ client/src/components/ui/categoryNav.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import './css/categoryNav.css'
 // массив для рендера категорий
 import { productCategories } from '../../data/categories/productCategories'
 
 const CategoryNav = () => {
-  const navigate = useNavigate()
   const { type } = useParams()
 
   // если выбрана категория, скроллить вверх
@@ -16,27 +15,21 @@ const CategoryNav = () => {
     })
   }
 
-  const navigateTo = (to) => {
-    navigate(to)
-  }
-
   return (
-    <div className='category-nav_HW'>
+    <div className='category-nav-hw'>
       <div className="my-container">
         <div className={'category-nav' + (type ? ' type-selected' : '')}>
-          {/* категории */}
+          {/* рендер категорий */}
           {productCategories.map(item => (
             item.active && (
               <div className={'category-nav-item' + (type === item.type ? ' active' : '')}>
                 <Link
                   key={item.id}
                   to={item.to}
-                  // className={'category-nav-item' + (type === item.type ? ' active' : '')}
-                  // className={'category-nav-item' + item.styleClass + (type === item.type ? ' active' : '')}
                 >
                   <img src={item.img} alt={item.alt} />
+                  <h3 >{item.label}</h3>
                 </Link>
-                <h3 onClick={() => navigateTo(item.to)}>{item.label}</h3>
               </div>
             )
           ))}
